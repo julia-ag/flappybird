0f93fde9e40b6cb2adae5868b101d52f997270d9
sophioca <0034209@sesi-es.org.br>
2023-05-10 09:42:03 -0300

    adicionei as variaveis do tubo

--- flappy.py
+++ flappy.py
@@ -27,3 +27,14 @@ bird_y_speed = 0
 #variaveis da janela
 playing_area_width = 300
 playing_area_height = 388
+
+#variaveis do tubo1
+pipe_width = 35
+pipe_space_height = 100
+pipe_space_y = 150
+pipe_x = playing_area_width-pipe_width
+
+#variaveis do tubo2
+pipe_space_height_2 = 100
+pipe_space_y_2 = 150
+pipe_x_2 = playing_area_width-pipe_width+200
