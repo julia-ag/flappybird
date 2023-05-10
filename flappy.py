d278ac6fcf35cd1de0c126fe4decc31eb5cc5baf
alexiamel <131879346+alexiamel@users.noreply.github.com>
2023-05-10 12:03:45 -0300

    adicionei o resto da programação

--- flappy.py
+++ flappy.py
@@ -39,3 +39,69 @@ def draw():
         ),
         color=(135,206,250)
     )
+    screen.draw.filled_rect(
+        Rect(
+            (bird_x, bird_y),
+            (bird_width, bird_height)
+        ),
+        color=(255,255,224)
+    )
+
+    #tubo1
+    desenhar_tubo(pipe_x,pipe_width,pipe_space_y,pipe_space_height)
+
+    #tubo2
+    desenhar_tubo(pipe_x_2,pipe_width,pipe_space_y_2,pipe_space_height_2)
+
+    #pontos na tela
+    screen.draw.text(str(points), (15,15))
+
+def on_key_down():
+    global bird_y_speed
+    if bird_y > 0:
+        bird_y_speed -= 165
+def update(dt):
+    global bird_y
+    global bird_y_speed
+    global points
+    global pipe_x
+    global pipe_x_2
+    bird_y_speed += 516 * dt
+    bird_y += bird_y_speed * dt
+    #testa se o tubo1 esta na tela
+    if( pipe_x > -35):
+        pipe_x -= 60 * dt
+    else:
+        points += 1
+        pipe_x = playing_area_width-pipe_width+54
+
+    #testa se o tubo2 esta na tela
+    if( pipe_x_2 > -35):
+        pipe_x_2 -= 60 * dt
+    else:
+        points += 1
+        pipe_x_2 = playing_area_width-pipe_width+54
+
+def desenhar_tubo(pipe_x,pipe_width,pipe_space_y,pipe_space_height):
+    #tubo1
+    screen.draw.filled_rect(
+        Rect(
+            (pipe_x, 0),
+            (pipe_width, pipe_space_y)
+    ),
+        color=(219,112,147)
+    )
+
+#tubo2
+    screen.draw.filled_rect(
+        Rect(
+            (pipe_x, pipe_space_y + pipe_space_height),
+            (pipe_width, pipe_space_y)
+         ),
+        color=(219,112,147)
+    )
+
+#config do pygame
+WIDTH = playing_area_width
+HEIGHT = playing_area_height
+
