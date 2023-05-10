c26a857175b0deb61ef283f95a1714d86a0d912c
alexiamel <131879346+alexiamel@users.noreply.github.com>
2023-05-10 09:46:23 -0300

    tirei a núvem

--- flappy.py
+++ flappy.py
@@ -5,21 +5,10 @@ import pygame
 #variaveis gerais
 points = 0
 
-# atores
-
-nuvem = Actor("nuvem")
-
 #variaveis passaros
 bird_x = 62
 bird_y = 200
 bird_width = 30
-bird_height = 25
-bird_y_speed = 0
-
-#variaveis nuvem
-nuvem_x = 62
-nuvem_y = 200
-nuvem_width = 30
 bird_height = 25
 bird_y_speed = 0
 
